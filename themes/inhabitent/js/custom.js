5d0898a2537f80fbcbbfb2b1f15f1ccadde59d1b
Jeff Liknes <jeff.liknes@gmail.com>
2018-11-09 13:31:46 -0800

    init

---
+++ themes/inhabitent/js/custom.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+(function($){
+
+    $(document).ready(function(){
+
+        // code goes here
+        // search form script
+
+
+    //todo add search form function 
+    //.focus and .blur
+$('body').append('hello');
+
+});
+
+})(jQuery);
+
+//
+
+//IIFE
+//IMMEDIATELY INVOKED FUNCTION EXPRESSION
