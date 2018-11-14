ce0c2319359dbd48af211be0bbb9d6b388d2fec8
Jeff Liknes <jeff.liknes@gmail.com>
2018-11-13 20:10:27 -0800

    organize templates, add find-us page and about

---
+++ themes/inhabitent/find-us.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the Find Us page.
+ *
+ * @package RED_Starter_Theme
+ * Template Name: Find-us Page Template
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
